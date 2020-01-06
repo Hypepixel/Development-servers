821e23077ab840bdbedeeb31de4df0e4bf6d898b
Jasper Drescher <jasperdre@gmail.com>
2020-01-06 15:14:40 +0100

    Update Install.bat

--- Server/Install.bat
+++ Server/Install.bat
@@ -2,7 +2,7 @@
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 pause
 set /p version=Which version would you like to install? (latest, 1.13.1, etc.):
-java -jar BuildTools.jar --rev %version%
+java -Xmx1024M -Xms1024M -jar BuildTools.jar --rev %version%
 pause
 for /r %%a in (*spigot-*) do (
     ren %%~nxa Spigot.jar
