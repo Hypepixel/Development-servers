f8ad445bd1dba616e497ca5169ddcd6cafa20721
Jasper Drescher <jasperdre@gmail.com>
2020-01-25 20:06:36 +0100

    Add remove step

--- Server/Install.bat
+++ Server/Install.bat
@@ -1,8 +1,10 @@
 @echo off
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 pause
-set /p version=Which version would you like to install? (latest, 1.13.1, etc.):
+set /p version=Which version would you like to install? (latest, 1.15.1, etc.):
 java -Xmx1024M -Xms1024M -jar BuildTools.jar --rev %version%
+pause
+del Spigot.jar
 pause
 for /r %%a in (*spigot-*) do (
     ren %%~nxa Spigot.jar
