1804503654cdd7e3fd68c5062574ae7b7d0e61f7
Jasper Drescher <jasperdre@gmail.com>
2020-01-25 20:07:51 +0100

    Change size

--- Server/Install.bat
+++ Server/Install.bat
@@ -2,7 +2,7 @@
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 pause
 set /p version=Which version would you like to install? (latest, 1.15.1, etc.):
-java -Xmx1024M -Xms1024M -jar BuildTools.jar --rev %version%
+java -Xms1G -Xmx1G -jar BuildTools.jar --rev %version%
 pause
 del Spigot.jar
 pause
