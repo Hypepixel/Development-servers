9343194d778f9f2c782cfc2d51cc75f6547ce56f
JasperDre <jasperdre@gmail.com>
2020-02-01 15:20:29 +0100

    Add directory change

--- spigot/start.bat
+++ spigot/start.bat
@@ -1,3 +1,2 @@
-@echo off
-java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar server/spigot.jar nogui
-pause
+cd server/
+start java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot.jar nogui
