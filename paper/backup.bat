0ffd7f196f231b56e35c72302c7018aec2a52400
JasperDre <jasperdre@gmail.com>
2020-05-15 17:20:22 +0200

    Added PaperMC

---
+++ paper/backup.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set source=%~dp0server/
+set /p destination=Please provide the absolute destination path:
+
+if exist %destination% goto GO
+:GO
+
+xcopy %source% %destination% /M /E /G /H /Y
+echo. 
+echo Finished copying %source% to %destination%
+echo.
+echo Created %destination%
+pause
---
+++ paper/install.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set buildpath=%~dp0build\
+set serverpath=%~dp0server\
+if not exist %buildpath% md %buildpath%
+if not exist %serverpath% md %serverpath%
+
+cd %buildpath%
+set /p version=Which version would you like to install? (latest, 1.15.2, etc.):
+curl -o paper.jar https://papermc.io/api/v1/paper/%version%/latest/download
+for /r %%a in (*paper-*) do (
+    ren %%~nxa paper.jar
+    echo renaming %%~nxa
+)
+
+cd %serverpath%
+del paper.jar
+cd ..
+move %buildpath%paper.jar %serverpath%paper.jar
+pause
